21d194e8c8a063ec8363d2b21f81f15d80e9cd84
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2026-06-25 23:04:08 -0300

    fixed KeyExpansion

--- rtl/KeyExpansion.vhdl
+++ rtl/KeyExpansion.vhdl
@@ -6,22 +6,28 @@ entity KeyExpansion is
         key_in_1, key_in_2, key_in_3, key_in_4 : in std_logic_vector(31 downto 0);
         key_out : out std_logic_vector(127 downto 0)
     );
-end entity KeyExpansion;
+end entity;
 
 architecture arch of KeyExpansion is
+
     signal GOut : std_logic_vector(31 downto 0);
-    signal xor1, xor2, xor3 : std_logic_vector(31 downto 0);
+    signal w0, w1, w2, w3 : std_logic_vector(31 downto 0);
+
 begin
+
     Gfunction : entity work.G_Function
         port map(
-            a => key_in_1,
-            z => GOut
+            a     => key_in_4,
+            round => "0001", -- se você já fixou isso internamente
+            z     => GOut
         );
-    
-    xor1 <= key_in_1 xor Gout;
-    xor2 <= xor1 xor key_in_2;
-    xor3 <= xor2 xor key_in_3;
 
-    key_out <= xor1 & xor2 & xor3 & (xor3 xor key_in_4);
-    
-    end architecture arch;
+
+    w0 <= key_in_1 xor GOut;
+    w1 <= key_in_2 xor w0;
+    w2 <= key_in_3 xor w1;
+    w3 <= key_in_4 xor w2;
+
+    key_out <= w0 & w1 & w2 & w3;
+
+end architecture;
---
+++ rtl/KeyRegisterBank.vhdl
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity KeyRegisterBank is
+    port (
+        clk        : in std_logic;
+        write_en   : in std_logic;
+        index      : in unsigned(3 downto 0);
+        key_in     : in std_logic_vector(127 downto 0);
+        key_out    : out std_logic_vector(127 downto 0)
+    );
+end entity;
+
+architecture arch of KeyRegisterBank is
+
+    type key_array is array (0 to 10) of std_logic_vector(127 downto 0);
+    signal mem : key_array;
+
+begin
+
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if write_en = '1' then
+                mem(to_integer(index)) <= key_in;
+            end if;
+        end if;
+    end process;
+
+    key_out <= mem(to_integer(index));
+
+end architecture;
